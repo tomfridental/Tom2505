ab6328b6fd8645ee6705bbb180e442b1a5e035dd
tomfridnetal@gmail.com <tomfridnetal@gmail.com>
2021-05-14 23:12:47 +0300

    app rename

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import regeneratorRuntime from "regenerator-runtime";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
 import { Provider } from 'react-redux'
 import configureStore from './state/store';
 
